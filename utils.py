2b8cb6cf2a09fa5ea4f582c1a14fab9c5949766f
Nihal Shetty <160050360+nihalshetty10@users.noreply.github.com>
2025-06-01 17:08:25 -0500

    Delete utils.py

--- utils.py
+++
@@ -1,9 +0,0 @@
-def american_to_prob(odds):
-    odds = float(odds)
-    if odds > 0:
-        return 100 / (odds + 100)
-    else:
-        return -odds / (-odds + 100)
-
-def format_date(dt):
-    return dt.strftime('%Y-%m-%d')
